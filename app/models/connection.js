11deb9e7c749af6445bd99d05b43108dd7f905d9
Clifford Patterson <cliff.patterson2@gmail.com>
2018-01-11 09:54:46 -0500

    make it better part duex

--- app/models/connection.js
+++ app/models/connection.js
@@ -3,8 +3,11 @@
 const mongoose = require('mongoose')
 const mUrl = 'mongodb://ad:min@ds249787.mlab.com:49787/cliff-project2'
 
-//mongoose.connect('mongodb://localhost/project-2-postNuke', {useMongoClient : true}) 
-mongoose.connect( mUrl , {useMongoClient : true}) 
+if (process.env.NODE_ENV == "production") {
+  mongoose.connect(process.env.MLAB_URL)
+} else {
+  mongoose.connect('mongodb://localhost/project-2-postNuke', {useMongoClient : true}) 
+}
 
 mongoose.Promise = Promise 
 
